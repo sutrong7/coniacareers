de07bc20c4819144b695dc6a74cea69d010f5851
sutrong7 <https://github.com/sutrong7>
2024-05-26 18:26:21 +0900

    first commit

---
+++ resource/js/common.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+    $(document).ready(function () {
+        const body = $('body');
+
+        // 1. 헤더 변경
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 0) {
+                $('.header').addClass('active');
+            } else {
+                $('.header').removeClass('active');
+            }
+        });
+
+        // 2. 모바일 헤더 노출
+        const html = $('html');
+        const moGnb = $('.mo_header');
+        const moGnbBtn = $('.gnb_btn');
+        moGnbBtn.on('click', function () {
+            if (moGnb.hasClass('active')) {
+                moGnb.removeClass('active');
+                html.removeClass('locked');
+            } else {
+                moGnb.addClass('active');
+                html.addClass('locked');
+            }
+        });
+
+        // 3. family site
+        const trigger = $('.family_site');
+        trigger.on('click', function () {
+            if ($(this).hasClass('on')) {
+                trigger.removeClass('on');
+            } else {
+                trigger.addClass('on');
+            }
+        });
+    });
+}());
+
+
